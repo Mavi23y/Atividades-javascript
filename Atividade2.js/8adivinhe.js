e9a957c84ecaa9b4980418182f19b05a7af00215
Maria Vitória <maria_vb_alves@estudante.sesisenai.org.br>
2025-11-03 15:56:02 -0300

    Implement guessing game logic in 8adivinhe.js

--- Atividade2.js/8adivinhe.js
+++ Atividade2.js/8adivinhe.js
@@ -1,36 +1,13 @@
-// EXEMPLO COM PROMPT-SYNC
 const prompt = require('prompt-sync')();
-const nome = prompt('digite swunome?');
-console.log(`seu nome é: ${nome}`);
 
-// let jogador = 20
-// let numeroAleatorio;
-// let contador = 0;
+let numSecreto = 20
+let active = true;
 
-// while (contador < 1) {
-
-//   numeroAleatorio = Math.floor(Math.random() * 30) ;
-//   console.log(`O Aleátorio foi: ${numeroAleatorio}`);
-//   contador++;
-
-//   if(jogador === numeroAleatorio){
-//     console.log("Você acertou!!")
-//   }
-// }
-
-// let jogador = 9
-// let numeroAleatorio;
-// let contador = 0;
-
-// while (contador < 10) {
-
-//   numeroAleatorio = Math.floor(Math.random() * 10) ;
-//   console.log(`O Aleátorio foi: ${numeroAleatorio}`);
-//   contador++;
-
-//   if(jogador === numeroAleatorio){
-//     console.log("Você acertou!!")
-//     break
-//   }
-
-// }
+while (active) {
+    let numDigitado = Number(prompt('Digite um número entre 1 e 30: '));
+    if (numDigitado === numSecreto) {
+        console.log("Parabéns! Você acertou o número secreto!");
+        active = false;
+    }
+    
+}
